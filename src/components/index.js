1cea79556550743aba62e6d766bd07543938d5da
Maks <maks135254@gmail.com>
2025-07-02 09:51:58 +0300

    Combine PR #1 and PR #2

--- src/components/index.js
+++
@@ -1,5 +0,0 @@
-export { default as Navbar } from './Navbar';
-export { default as FilterPanel } from './FilterPanel';
-export { default as List } from './List';
-export { default as TrackingForm } from './TrackingForm';
-export { default as Chart } from './Chart';
